9056c9292ff53f8b15880591db6980b75247df8c
CharleneYuan <953812979@qq.com>
2017-06-01 09:21:18 +0800

    2017.6.1-9.21
    更改手机号验证码验证方式

--- src/servlet/OrderServlet.java
+++ src/servlet/OrderServlet.java
@@ -1,11 +1,19 @@
 package servlet;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import net.sf.json.JSONObject;
+import Bean.UserBean;
+import Service.LogService;
 
 /**
  * Servlet implementation class OrderServlet
@@ -13,27 +21,61 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/OrderServlet")
 public class OrderServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+	private static final int SUCCESS = 1;//成功
+	private static final int ERROR = 0;//密码错误
+	private static final int CONFILICT = -1;//账号冲突或不存在
+	
+	private JSONObject jsonObject;
+	private LogService logService;
+	private UserBean userBean;
+	private JSONObject jsonReply = new JSONObject();//封装服务器返回的JSON对象   
+	
     public OrderServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		response.setCharacterEncoding("UTF-8");
+	    response.setContentType("text/json");
+		StringBuffer sb = new StringBuffer("");
+		String result = "";
+		
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					request.getInputStream(), "utf-8"));
+			String temp;
+			while ((temp = br.readLine()) != null) {
+				sb.append(temp);
+			}
+			br.close();
+			result = sb.toString();
+			//打印android端上传的JSON数据
+			System.out.println(result);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		jsonObject = JSONObject.fromObject(result);
+		String type = jsonObject.getString("type");
+		logService = new LogService(); //新建Service实例
+		PrintWriter pw = response.getWriter();
+		
+		if(type.equals("emailLogin")){//邮件登录
+			emailLogin();
+		}else if(type.equals("phoneLogin")){
+			phoneLogin();
+		}else if(type.equals("register")){
+			register();
+		}
+	
+		System.out.println(jsonReply);
+		pw.write(jsonReply.toString());
+		pw.flush();
+		pw.close();
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+
 	}
 
 }
